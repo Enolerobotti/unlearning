4fd08a15de83e7da0f0107e7ebd7403041f53909
Artem Pavlovskii <apavlovs@its.jnj.com>
2023-08-27 13:46:45 +0200

    attempt to poisson data

--- unlearning/grad_per_sample.py
+++ unlearning/grad_per_sample.py
@@ -6,6 +6,7 @@ from copy import deepcopy
 from torch.nn import BCEWithLogitsLoss, MSELoss
 from torch.optim import Adam
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score as roc_auc
 from unlearning.net import Net
 from unlearning.controller import Controller
 
@@ -28,6 +29,15 @@ class Model:
         loss_fn = BCEWithLogitsLoss(reduction='none')
         c_loss_fn = MSELoss()
         eval_loss = float('inf')
+        stat = {
+            'epoch': [],
+            'roc_auc': [],
+            'train_loss': [],
+            'eval_loss': [],
+            'controller_epochs': [],
+            'controller_train_mse': [],
+            'controller_eval_mse': []
+        }
         for e in range(self.epochs):
             net.train()
             optimizer.zero_grad()
@@ -40,34 +50,58 @@ class Model:
 
             sample_grads = torch.concat(_sample_grads)
             external_grad = sample_grads.mean()
-            loss.mean().backward(gradient=external_grad)
+            train_loss = loss.mean()
+            train_loss.backward()#gradient=external_grad)
             optimizer.step()
 
             net.eval()
             e_y_hat = net(x_test)
-            _eval_loss = loss_fn(e_y_hat, y_test)
+            _eval_loss = loss_fn(e_y_hat, y_test).mean()
+            auc = roc_auc(y_test.cpu().numpy(), e_y_hat.detach().cpu().numpy())
+            stat['epoch'].append(e)
+            stat['roc_auc'].append(auc)
+            stat['train_loss'].append(train_loss.item())
+            stat['eval_loss'].append(_eval_loss.item())
             if _eval_loss < eval_loss:
                 eval_loss = _eval_loss
                 self.net = deepcopy(net)
 
             # train the controller
-            c_features = torch.concat([torch.ones(size=(len(x), 1)) * e / self.epochs, x], dim=1)
+            c_features = self.grad_features(x, e)
 
             c_train, c_test, g_train, g_test = train_test_split(c_features, sample_grads.detach())
 
-            controller.train()
+            controller_loss_diff = float('inf')
+            patience = 10
+            report_c_epoch = self.controller_epochs
             for ce in range(self.controller_epochs):
+                controller.train()
                 c_optimizer.zero_grad()
                 g_hat = controller(c_train)
-                c_loss = c_loss_fn(g_hat, g_train)
-                c_loss.backward()
+                c_train_loss = c_loss_fn(g_hat, g_train)
+                c_train_loss.backward()
                 c_optimizer.step()
 
-            controller.eval()
-            e_g_hat = controller(c_test)
-            c_eval_loss = c_loss_fn(e_g_hat, g_test)
-            self.controller = deepcopy(controller)
-            # print(c_eval_loss.item())
+                controller.eval()
+                e_g_hat = controller(c_test)
+                c_eval_loss = c_loss_fn(e_g_hat, g_test)
+                c_loss_diff = torch.abs(c_eval_loss - c_train_loss).item()
+                if c_loss_diff < controller_loss_diff:
+                    controller_loss_diff = c_loss_diff
+                else:
+                    patience -= 1
+
+                if patience == 0:
+                    report_c_epoch = ce
+                    break
+            stat['controller_epochs'] = report_c_epoch
+            stat['controller_train_mse'].append(c_train_loss.item())
+            stat['controller_eval_mse'].append(c_eval_loss.item())
+        self.controller = deepcopy(controller)
+        return stat
+
+    def grad_features(self, x: Tensor, epoch: int) -> Tensor:
+        return torch.concat([torch.ones(size=(len(x), 1)) * (self.epochs - epoch) / self.epochs, x], dim=1)
 
     def predict(self, x: Tensor):
         self.net.eval()
--- unlearning/main.py
+++ unlearning/main.py
@@ -1,10 +1,13 @@
 import torch
+from torch.nn import Parameter
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
+import pandas as pd
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 # from unlearning.classic_model import Model
 from unlearning.grad_per_sample import Model
@@ -17,25 +20,49 @@ np.random.seed(seed)
 def main():
     df = load_iris(as_frame=True)
     data = df.frame
+
+    poisson = data[data.target == 2].sample(frac=1)
+    poisson.target = 1.0
+
     data = data[data.target != 2]
+
     train_df, test_df = train_test_split(data)
+
+    union_train_df = pd.concat([train_df, poisson])
+
     scaler = StandardScaler()
     train_f = scaler.fit_transform(train_df.iloc[:, :-1].values)
-    train_t = train_df.target.values
+    poisson_f = scaler.transform(poisson.iloc[:, :-1].values)
+    train_f = np.concatenate([train_f, poisson_f])
+    train_t = union_train_df.target.values
     test_f = scaler.transform(test_df.iloc[:, :-1].values)
     test_t = test_df.target.values
     train_features = torch.tensor(train_f, dtype=torch.float32)
     train_targets = torch.tensor(train_t, dtype=torch.float32).unsqueeze(dim=1)
     test_features = torch.tensor(test_f, dtype=torch.float32)
     test_targets = torch.tensor(test_t, dtype=torch.float32).unsqueeze(dim=1)
-    model = Model(500)
+    n_epochs = 150
+    model = Model(n_epochs)
     stats = model.fit(train_features, train_targets, test_features, test_targets)
     y_hat = model.predict(test_features)
-    # roc = roc_auc_score(test_t, y_hat)
-    # stats['roc'] = roc
-    importance = None# model.importance
-    # grouth_rate = np.diff(importance, axis=0)
-    return stats, importance
+    roc = roc_auc_score(test_t, y_hat)
+    stats['roc'] = roc
+
+    # Cure the model / unlearn
+    poisson_samples = scaler.transform(poisson.iloc[:, :-1].values)
+    poisson_features = torch.tensor(poisson_samples, dtype=torch.float32)
+    for e in range(n_epochs):
+        features = model.grad_features(poisson_features, e)
+        grad_per_sample = model.predict_grad(features)
+        grad = grad_per_sample.mean(dim=0)
+        model.net.layer.weight = Parameter(model.net.layer.weight - grad * 0.003)  # TODO this is not I was gonna do. Use my formula instead
+
+    y_hat = model.predict(test_features)
+    roc2 = roc_auc_score(test_t, y_hat)
+    stats['roc_cured'] = roc2
+
+
+    return stats, None
 
 
 def plot_learning_curves(stat: dict):
@@ -48,7 +75,11 @@ def plot_learning_curves(stat: dict):
 
 if __name__ == '__main__':
     stat, imp = main()
-    # print(f"AUC {stat['roc']}")
+    df = pd.DataFrame(stat)
+    # print(df)
+    # print(f"AUC {stat['roc_auc'][-1]}")
+    print(f"AUC {stat['roc']}")
+    print(f"AUC {stat['roc_cured']}")
     # imp is a sample importance
     # plot_learning_curves(stat)
 
